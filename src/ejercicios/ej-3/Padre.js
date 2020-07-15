8173e2a92defe348418e9f7041ce9041e3de3c59
Suso <susopc1981@hotmail.com>
2020-07-15 11:42:17 +0200

    ex3 done

--- src/ejercicios/ej-3/Padre.js
+++ src/ejercicios/ej-3/Padre.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import Hijo from "./Hijo";
 
 const Padre = () => {
-    const printCustomText = (text) => {
-        console.log(text);
-    };
-    
-    return (
-        <div className="alert alert-primary">
-            <h4>Padre</h4>
-            <Hijo />
-        </div>
-    );
+  const printCustomText = (text) => {
+    console.log(text);
+  };
+
+  return (
+    <div className="alert alert-primary">
+      <h4>Padre</h4>
+      <Hijo data={printCustomText} />
+    </div>
+  );
 };
 
 export default Padre;
